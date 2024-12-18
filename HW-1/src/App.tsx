9661948f3570dbb5b8d15187e91837a69f1c3535
Osama Maree <osamamarei2001@gmail.com>
2024-12-18 20:50:27 +0200

    HW-1: Add Base Project(Setup files"

---
+++ HW-1/src/App.tsx
@@ -0,0 +1,14 @@
+import Box from "@mui/material/Box"
+import { lazy, Suspense } from "react"
+const Calculater=lazy(()=>import("./pages/calculator"))
+
+function App() {
+
+  return (
+ <Suspense fallback={<Box>Loading...</Box>}>
+     <Calculater />
+ </Suspense>
+  )
+}
+
+export default App
